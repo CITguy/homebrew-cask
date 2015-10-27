029c7c878c922acf07aee9d8b7ca2cc817b14941
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-10-27 15:15:09 -0500

    first commit

---
+++ selenium-server-standalone2471.rb
@@ -0,0 +1,45 @@
+class SeleniumServerStandalone2471 < Formula
+  desc "Automated Browser Control (2.47.1)"
+  homepage "http://seleniumhq.org/"
+  url "https://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar"
+  sha256 "84efe8684b4b738dec40f071178504a6f001ad7ab75df737a5c1faec3036fa7c"
+
+  conflicts_with "selenium-server-standalone", :because => "Differing version of core formula"
+
+  def install
+    libexec.install "selenium-server-standalone-2.47.1.jar"
+    bin.write_jar_script libexec/"selenium-server-standalone-2.47.1.jar", "selenium-server"
+  end
+
+  plist_options :manual => "selenium-server -p 4444"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>/usr/bin/java</string>
+          <string>-jar</string>
+          <string>#{libexec}/selenium-server-standalone-2.47.1.jar</string>
+          <string>-port</string>
+          <string>4444</string>
+        </array>
+        <key>ServiceDescription</key>
+        <string>Selenium Server</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/selenium-error.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/selenium-output.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+end
